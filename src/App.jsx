b5577b71712e2ebcf6340a8d9ad64247fc2a6f60
Aga Siwek <176235224+aga-siwek@users.noreply.github.com>
2025-02-03 09:19:20 +0100

    correct role

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,17 @@ function App() {
           </div>
         </header>
         {/* review */}
-        <div className='reviews' role="main">
-          <div className='reviews__first-review' role="contentinfo"><Review star={5} place="Reviews" /></div>
-          <div className='reviews__second-review' role="contentinfo"><Review star={5} place="Report Guru" /></div>
-          <div className='reviews__third-review' role="contentinfo"><Review star={5} place="BestTech" /></div>
+        <div className='reviews' role="contentinfo">
+          <div className='reviews__first-review' ><Review star={5} place="Reviews" /></div>
+          <div className='reviews__second-review' ><Review star={5} place="Report Guru" /></div>
+          <div className='reviews__third-review' ><Review star={5} place="BestTech" /></div>
         </div>
       </div>
       {/* opinion */}
       <div className='opinions' role="main">
-        <div className='opinions__first-opinion' role="contentinfo"><Opinion avatar={avatar_colton} name="Colton Smith" verified={true} notice="We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!" /></div>
-        <div className='opinions__second-opinion' role="contentinfo"><Opinion avatar={avatar_irene} name="Irene Roberts" verified={true} notice="Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery." /></div>
-        <div className='opinions__third-opinion' role="contentinfo"><Opinion avatar={avatar_anne} name="Anne Wallace" verified={true} notice="Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!" /></div>
+        <div className='opinions__first-opinion'><Opinion avatar={avatar_colton} name="Colton Smith" verified={true} notice="We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!" /></div>
+        <div className='opinions__second-opinion'><Opinion avatar={avatar_irene} name="Irene Roberts" verified={true} notice="Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery." /></div>
+        <div className='opinions__third-opinion' ><Opinion avatar={avatar_anne} name="Anne Wallace" verified={true} notice="Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!" /></div>
       </div>
     </div>
   )
